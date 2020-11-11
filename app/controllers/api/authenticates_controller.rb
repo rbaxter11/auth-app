77f34ebbf7ac459e7707f5c9d7354d10ae449f9b
ryan <ryanandrewbaxter@gmail.com>
2020-11-10 20:07:32 -0600

    Creates app with authentication

---
+++ app/controllers/api/authenticates_controller.rb
@@ -0,0 +1,2 @@
+class Api::AuthenticatesController < ApplicationController
+end
---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,25 @@
+class Api::UsersController < ApplicationController
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def show
+    if current_user
+      input = params[:id]
+      @user = User.find_by(id: input)
+      render "show.json.jb"
+    else
+      render json: { message: "Please log in" }
+    end
+  end
+end
